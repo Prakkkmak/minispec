24e05df6017be852ee17fb4ae0fc57032e9cb2f6
Prakkmak <prakkmak@gmail.com>
2021-03-04 10:01:10 +0100

    Modify entity
    Add fields and accessors 
    Add Attribute class

--- src/com/company/minispec/model/Entity.java
+++ src/com/company/minispec/model/Entity.java
@@ -1,4 +1,23 @@
 package com.company.minispec.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Entity {
+    protected String name;
+    protected List<Attribute> attributes;
+
+    public Entity(String name){
+        attributes = new ArrayList<>();
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public List<Attribute> getAttributes(){
+        return this.attributes;
+    }
 }
+
